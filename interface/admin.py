957207069474dca9722594654c67868703f2bd7d
zakhep66 <sur.mikhael@gmail.com>
2022-07-02 02:48:07 +0300

    Added interface is django admin panel and Meta class, str method for models. Generated uuid in models

--- interface/admin.py
+++ interface/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+admin.site.register(Medication)
+admin.site.register(MedOrder)
+admin.site.register(LabEvent)
+admin.site.register(LabType)
+admin.site.register(Patient)
+admin.site.register(PatientIcd)
+admin.site.register(Therapy)
+admin.site.register(Prescription)
+admin.site.register(IndexType)
+admin.site.register(User)
+admin.site.register(MedIndex)
+admin.site.register(OrderEntry)
