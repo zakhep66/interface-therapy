7bcec9627db0a79849b30884cc81a4ed90786e50
zakhep66 <sur.mikhael@gmail.com>
2022-07-03 22:53:12 +0300

    some commit

--- core/settings.py
+++ core/settings.py
@@ -137,11 +137,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-# STATIC_URL = 'static/'
-# MEDIA_URL = 'media/'
-#
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # CORS_ALLOWED_ORIGINS = [
